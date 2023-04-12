16bdb96a5b959d15f3fcedbb963c008bbe78ab59
mikasiddiqui <mikasiddiqui@gmail.com>
2023-04-12 13:09:56 +1000

    Testing explainer GPU

--- predict.py
+++ predict.py
@@ -10,6 +10,7 @@ import tensorflow as tf
 import json
 from sentence_transformers import SentenceTransformer, util
 from cog import BasePredictor, Input
+from lime.lime_text import LimeTextExplainer
 from typing import Any
 
 nltk.download("stopwords")
@@ -50,6 +51,15 @@ class Predictor(BasePredictor):
         result = util.pytorch_cos_sim(embeddings[0], embeddings[1])
         return float(result)
 
+    def predict_proba_hashtags(self, arr):
+        processed=[]
+        for i in arr:
+            processed.append(i)
+        id,mask=self.bert_encode(self.tokenizer, processed, 60)
+        pred=self.hashtag_model.predict([id,mask])
+        format_pred = np.concatenate([1.0-pred, pred], axis=1)
+        return format_pred
+
     def predict(
         self, 
         concept: str = Input(description="Concept to compare the hashtag generations with"),
@@ -62,10 +72,22 @@ class Predictor(BasePredictor):
         sim_score = self.get_similarity(concept, hashtags_cleaned)
         if sim_score >= 0.6:
             grade = self.hashtag_model.predict((input_id,attention_mask))[0][0]*10*369
-            response_dict = {'hashtags': hashtags_list,'score': float(grade)}
+            class_names = ['Low engagement', 'High engagement']
+            explainer = LimeTextExplainer(class_names=class_names)
+            exp = explainer.explain_instance(hashtags_cleaned, self.predict_proba_hashtags, num_samples=20000)
+            explainer_dict = [{'word': str(word), 'score': float(score)} for word,score in exp.as_list()]
+            explainer_dict = sorted(explainer_dict, key=itemgetter('score'), reverse=True)
+            explainer_dict =[dict(explainer_dict[i], **{'position':i+1}) for i in range(len(explainer_dict))]
+            response_dict = {'hashtags': hashtags_list,'score': float(grade), 'analysis' :explainer_dict}
 
         else:
             grade = self.hashtag_model.predict((input_id,attention_mask))[0][0]*10*369*(1 - sim_score)*-1
-            response_dict = {'hashtags': hashtags_list,'score': float(grade)}
+            class_names = ['Low engagement', 'High engagement']
+            explainer = LimeTextExplainer(class_names=class_names)
+            exp = explainer.explain_instance(hashtags_cleaned, self.predict_proba_hashtags, num_samples=20000)
+            explainer_dict = [{'word': str(word), 'score': float(score)} for word,score in exp.as_list()]
+            explainer_dict = sorted(explainer_dict, key=itemgetter('score'), reverse=True)
+            explainer_dict =[dict(explainer_dict[i], **{'position':i+1}) for i in range(len(explainer_dict))]
+            response_dict = {'hashtags': hashtags_list,'score': float(grade), 'analysis' :explainer_dict}
 
         return response_dict
